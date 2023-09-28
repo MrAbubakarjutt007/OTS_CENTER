5cf130c2bb23573039398c38acc628a828104418
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-09-28 01:23:46 -0700

    ADDING DATA

--- src/components/GoToTop.js
+++ src/components/GoToTop.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import { FaArrowUp } from "react-icons/fa";
 
 const GoToTop = () => {
   const GoToBotton =()=>{
@@ -7,7 +8,43 @@ const GoToTop = () => {
     window.scrollTo({top:0,left:0, behavior:'smooth'});
   }
   const Wrapper =styled.section`
+      diplay:flex;
+      justify-content:center;
+      align-items:center;
+      .top-botton{
+
+        font-size:2.4rem;
+        width:6rem;
+        height:6rem;
+        color:#fff;
+        background-color:${({theme})=>theme.colors.btn};
+        box-shadow:${({theme})=>theme.colors.shadow};
+        border-radius:50%;
+        position:fixed;
+        bottom:5rem;
+        right:5rem;
+        z-index:999;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        cursor:pointer;
+        
+        &--icon{
+          animation:GoToBotton 1.2s linear infinite alternate-reverse;
+      }
+      @keyframes GoToBotton{
+      0%{
+        transform:translatey(-0.5rem);
+      }
+      100%{
+        transform:translatey(1rem);
+      }
+
+      }
+
+      }
       
+
   
 
   `;
@@ -15,7 +52,7 @@ const GoToTop = () => {
   return (
     <Wrapper>
     <div className='top-botton' onClick={GoToBotton}>
-         
+         <FaArrowUp className='top-botton--icon'/>                  
     </div>
     </Wrapper>
   )
